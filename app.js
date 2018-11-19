0956d43af48b232ba4dceb0d46bdf6547b650d4d
Rokas Grigaravicius <grigroka@gmail.com>
2018-11-19 11:55:56 +0200

    addBookToList UI method

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ function Book(title, author, isbn) {
 // UI Constructor
 function UI() {}
 
+UI.prototype.addBookToList = function(book) {
+  const list = document.querySelector('#book-list');
+  // Create tr element
+  const row = document.createElement('tr');
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X<a/></td>
+  `;
+  list.appendChild(row);
+};
+
 // Event Listeners
 document.querySelector('#book-form').addEventListener('submit', function(e) {
   // Get form values
